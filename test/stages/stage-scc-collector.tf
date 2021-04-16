74dc2bae3dcd35de6407e157d641e0f82f073428
Sean Sundberg <seansund@us.ibm.com>
2021-04-16 00:10:04 -0500

    Removes unneeded name_prefix
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage-scc-collector.tf
+++ test/stages/stage-scc-collector.tf
@@ -3,7 +3,6 @@ module "scc-collector" {
 
   resource_group_id    = module.resource_group.id
   region               = var.region
-  name_prefix          = var.name_prefix
   ibmcloud_api_key     = var.ibmcloud_api_key
   vpc_name             = module.vpc.name
   vpc_subnet_count     = module.subnets.count
